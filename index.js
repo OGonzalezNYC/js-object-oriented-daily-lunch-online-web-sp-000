340879f04978992e83cfccd27421a3222b7aedd7
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-12-01 05:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ class Delivery {
   
   meal() {
     return store.meals.filter(function(meal) {
-      return this.mealId === meal.id;
+      this.mealId === meal.id;
     }.bind(this))
   }
   
